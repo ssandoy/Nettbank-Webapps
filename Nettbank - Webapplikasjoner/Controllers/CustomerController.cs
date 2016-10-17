9724e7d4d789016c0f127b3c7c5e0afbc4176ff1
ssandoy <sanderfsandoy@hotmail.com>
2016-10-17 11:34:47 +0200

    Navbar & Layout-file

---
+++ Nettbank - Webapplikasjoner/Controllers/CustomerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Nettbank___Webapplikasjoner.Models;
+
+namespace Nettbank___Webapplikasjoner.Controllers
+{
+    public class CustomerController : Controller
+    {
+       
+    }
+}
