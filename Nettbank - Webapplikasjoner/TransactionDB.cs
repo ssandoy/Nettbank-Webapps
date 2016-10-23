58a31306765db51c4372e3d8586117253ecd4fd0
Sindre <sindre.beba@gmail.com>
2016-10-23 17:32:28 +0200

    Lagt til validering ved endring av transaksjoner.

--- Nettbank - Webapplikasjoner/TransactionDB.cs
+++ Nettbank - Webapplikasjoner/TransactionDB.cs
@@ -5,22 +5,16 @@ using System.Linq;
 using System.Web;
 using Nettbank___Webapplikasjoner.Models;
 
-namespace Nettbank___Webapplikasjoner
-{
-    public class TransactionDB
-    {
+namespace Nettbank___Webapplikasjoner {
+    public class TransactionDB {
         HttpContext context = HttpContext.Current;
 
-        public List<Transaction> listTransactions(string accountNumber)
-        {
-            using (var db = new DbModel())
-            {
+        public List<Transaction> listTransactions(string accountNumber) {
+            using (var db = new DbModel()) {
                 var allTransactions = db.transactions.Where(t => t.accountNumber == accountNumber);
                 var transactions = new List<Transaction>();
-                foreach (var t in allTransactions)
-                {
-                    transactions.Add(new Transaction
-                    {
+                foreach (var t in allTransactions) {
+                    transactions.Add(new Transaction {
                         transactionId = t.transactionID,
                         amount = t.amount,
                         timeToBeTransfered = t.timeToBeTransfered,
@@ -34,18 +28,13 @@ namespace Nettbank___Webapplikasjoner
             }
         }
 
-        public List<Transaction> listExecutedTransactions(string accountNumber)
-        {
-            using (var db = new DbModel())
-            {
+        public List<Transaction> listExecutedTransactions(string accountNumber) {
+            using (var db = new DbModel()) {
                 var allTransactions = db.transactions.Where(t => t.accountNumber == accountNumber || t.toAccountNumber == accountNumber);
                 var transactions = new List<Transaction>();
-                foreach (var t in allTransactions)
-                {
-                    if (t.timeTransfered != null)
-                    {
-                        transactions.Add(new Transaction
-                        {
+                foreach (var t in allTransactions) {
+                    if (t.timeTransfered != null) {
+                        transactions.Add(new Transaction {
                             transactionId = t.transactionID,
                             amount = t.amount,
                             timeTransfered = t.timeTransfered,
@@ -78,11 +67,12 @@ namespace Nettbank___Webapplikasjoner
                     // Validerer utførelsesdato.
                     if (newTransaction.timeToBeTransfered == null) {
                         newTransaction.timeToBeTransfered = DateTime.Now;
-                    } else if (newTransaction.timeToBeTransfered.Value.CompareTo(DateTime.Now) < 0) {
+                    }
+                    else if (newTransaction.timeToBeTransfered.Value.CompareTo(DateTime.Now) < 0) {
                         return "Utførelsesdatoen på være en dato i fremtiden. La feltet stå tomt for å utføre betalingen umiddelbart.";
                     }
 
-                    // Setter og validerer fra-kontonummer.
+                    // Validerer fra-kontonummer.
                     var account = db.accounts.FirstOrDefault(a => a.accountNumber == t.fromAccountNumber);
                     if (account == null) {
                         return "Kontoen du vil betale fra eksisterer ikke";
@@ -102,65 +92,77 @@ namespace Nettbank___Webapplikasjoner
             }
         }
 
-        public bool deleteTransaction(int id)
-        {
-            using (var db = new DbModel())
-            {
-                try
-                {
-                    Transactions deleteTransaction = db.transactions.Find(id);
+        public bool deleteTransaction(int id) {
+            using (var db = new DbModel()) {
+                try {
+                    var deleteTransaction = db.transactions.Find(id);
                     db.transactions.Remove(deleteTransaction);
                     db.SaveChanges();
                     return true;
-                }
-                catch (Exception feil)
-                {
+                } catch (Exception exc) {
                     return false;
                 }
             }
         }
 
-        public Transactions findTransanction(int id)
-        {
-            using (var db = new DbModel())
-            {
-                try
-                {
-                    Transactions transaction = db.transactions.Find(id);
-                    if (transaction != null)
-                        return transaction;
-                    else
-                        return null;
-                }
-                catch (Exception e)
-                {
+        public Transactions findTransanction(int id) {
+            using (var db = new DbModel()) {
+                try {
+                    var transaction = db.transactions.Find(id);
+                    return transaction ?? null;
+                } catch (Exception exc) {
                     return null;
                 }
             }
         }
 
-        public bool updateTransaction(Transaction t)
-        {
-            using (var db = new DbModel())
-            {
-                try
-                {
-                    Transactions transactions = db.transactions.Find(t.transactionId);
+        public string updateTransaction(Transaction t) {
+            using (var db = new DbModel()) {
+                try {
+                    var transactions = db.transactions.Find(t.transactionId);
                     transactions.toAccountNumber = t.toAccountNumber;
-                    transactions.accountNumber = t.fromAccountNumber;
                     transactions.amount = t.amount;
                     transactions.comment = t.comment;
-                    if (t.timeToBeTransfered != null)
-                    {
-                        transactions.timeToBeTransfered = t.timeToBeTransfered;
+
+                    // Validerer beløp.
+                    if (transactions.amount <= 0) {
+                        return "Beløpet må være positivt.";
                     }
-                    db.Entry(transactions).State = EntityState.Modified;
+
+                    // Setter og validerer utførelsesdato.
+                    if (t.timeToBeTransfered != null) {
+                        if (t.timeToBeTransfered.Value.CompareTo(DateTime.Now) > 0) {
+                            transactions.timeToBeTransfered = t.timeToBeTransfered;
+                        } else {
+                            return "Utførelsesdatoen på være en dato i fremtiden. La feltet stå tomt for å ikke endre på datoen.";
+                        }
+                    }
+
+                    // Validerer til-kontonummer.
+                    if (db.accounts.FirstOrDefault(a => a.accountNumber == t.toAccountNumber) == null) {
+                        return "Kontoen du vil betale til eksisterer ikke";
+                    }
+
+                    // Validerer og setter fra-kontonummer hvis det er endret.
+                    if (transactions.accountNumber != t.fromAccountNumber) {
+                        var oldAccount = transactions.account;
+                        var newAccount = db.accounts.FirstOrDefault(a => a.accountNumber == t.fromAccountNumber);
+
+                        if (newAccount == null) {
+                            return "Kontoen du vil betale fra eksisterer ikke";
+                        }
+
+                        oldAccount.transactions.Remove(transactions);
+                        newAccount.transactions.Add(transactions);
+                    } else {
+                        db.Entry(transactions).State = EntityState.Modified;
+                    }
+                    
                     db.SaveChanges();
-                    return true;
+                    return "";
                 }
-                catch (Exception e)
-                {
-                    return false;
+                catch (Exception exc) {
+                    return "Feil: " + exc.Message;
                 }
             }
         }
