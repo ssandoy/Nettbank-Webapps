8e0cc11651c2fc070ce43e8992a1aa004dfc36a4
ssandoy <sanderfsandoy@hotmail.com>
2016-11-10 19:29:18 +0100

    Generere helt random tall for BankID

--- BLL/BankIdLogic.cs
+++ BLL/BankIdLogic.cs
@@ -2,15 +2,13 @@
 
 namespace BLL {
     public class BankIdLogic {
-        private static readonly string[] BankId = {
-            "345281", "235152", "852352", "512512", "396934", "991231", "712314", "641241",
-            "123211", "151255"
-        };
+      
 
         public static string GetBankId() {
             var rnd = new Random();
-            var randomIndex = rnd.Next(0, BankId.Length);
-            return BankId[randomIndex];
+            var x = rnd.Next(0, 1000000);
+            string s = x.ToString("000000");
+            return s;
         }
     }
 }
